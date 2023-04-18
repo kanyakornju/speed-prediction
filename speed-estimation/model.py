cbab9647f257394208f9c7d0e662212b0680670f
Kanyakorn Jumangmor <117687202+kanyakornju@users.noreply.github.com>
2023-04-18 16:41:05 +0100

    Add folder

---
+++ speed-estimation/model.py
@@ -0,0 +1,33 @@
+from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D, Dropout, Reshape, BatchNormalization, ELU
+from keras.optimizers import Adam
+
+
+def CNNModel():
+    model = Sequential()
+    model.add(Conv2D(24, (5, 5), input_shape = (240, 320, 3), strides=(2,2)))
+    model.add(ELU())
+    model.add(Conv2D(36, (5, 5), strides=(2,2)))
+    model.add(ELU())
+    model.add(Conv2D(48, (5, 5), strides=(2,2)))
+    model.add(ELU())
+    model.add(Dropout(0.5))
+    model.add(Conv2D(64, (3, 3), strides= (1,1)))
+    model.add(ELU())
+    model.add(Conv2D(64, (3, 3), strides= (1,1), padding = 'valid'))
+    model.add(Flatten())
+    model.add(ELU())
+    model.add(Dense(100))
+    model.add(ELU())
+    model.add(Dense(50))
+    # model.add(Dropout(0.5))
+    model.add(ELU())
+    model.add(Dense(10))
+    model.add(ELU())
+    model.add(Dense(1))
+
+    adam = Adam(lr=1e-4)
+    model.compile(optimizer = adam, loss = 'mse')
+
+    return model
