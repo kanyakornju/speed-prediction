8dd3f6f462c4f9366afd6bcc584d1b96f12df009
Kanyakorn Jumangmor <117687202+kanyakornju@users.noreply.github.com>
2023-04-22 17:50:39 +0100

    Update train.py

--- distance-estimation/train.py
+++ distance-estimation/train.py
@@ -41,8 +41,11 @@ def main():
     # create model
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(64, input_dim=4, kernel_initializer='normal', activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(32, kernel_initializer='normal', activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(16, kernel_initializer='normal', activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(1, kernel_initializer='normal'))
 
     model.compile(loss='mean_squared_error', optimizer='adam')
